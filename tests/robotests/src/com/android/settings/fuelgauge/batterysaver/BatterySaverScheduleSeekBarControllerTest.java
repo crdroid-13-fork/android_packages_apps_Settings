4fd3ae92c1788a2abd264c15109951442a75e37d
Henrik Baard <henrik.baard@sony.com>
2025-02-14 08:15:55 +0100

    Change range of battery saver schedule seekbar
    The minimum value which can be set as trigger for
    Battery saver was changed from 10% -> 20% however the
    settings UI was not aligned with this change.
    
    ref: b0fe225 Update the battery saver scheduler minimum level
    
    Update the UI to align with the minimum trigger
    level.
    
    Bug: 396444535
    Test: manual, check minimum seekbar value after reboot.
    Test: atest BatterySaverScheduleSeekBarControllerTest
    Google: 3494973
    Change-Id: I4baa766ff1cb78ce656ae340ce930094b9e124e0

--- tests/robotests/src/com/android/settings/fuelgauge/batterysaver/BatterySaverScheduleSeekBarControllerTest.java
+++ tests/robotests/src/com/android/settings/fuelgauge/batterysaver/BatterySaverScheduleSeekBarControllerTest.java
@@ -68,7 +68,7 @@ public class BatterySaverScheduleSeekBarControllerTest {
 
     @Test
     public void updateSeekBar_percentageMode_hasCorrectProperties() {
-        final CharSequence expectedTitle = "10%";
+        final CharSequence expectedTitle = "20%";
         Settings.Global.putInt(mResolver, Global.AUTOMATIC_POWER_SAVE_MODE,
                 PowerManager.POWER_SAVE_MODE_TRIGGER_PERCENTAGE);
         Settings.Global.putInt(mResolver, Global.LOW_POWER_MODE_TRIGGER_LEVEL, 10);
